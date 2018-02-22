d8ea57ca71c519c2e02ef4e2a20c952bede2c889
Mufti <muftiismirizqi@gmail.com>
2018-02-22 13:53:59 +0700

    fixing halaman pencarian

--- application/models/Home_Model.php
+++ application/models/Home_Model.php
@@ -33,6 +33,7 @@
 			$this->db->from('rute');
 			$this->db->where('rute_from', $this->input->get('rute_from'));
 			$this->db->where('rute_to', $this->input->get('rute_to'));
+			$this->db->like('reservation.depart_at', $this->input->get('depart_at'));
 			$this->db->join('reservation', 'rute.id_rute = reservation.id_rute');
 			return $this->db->get()->row();
 		}
